a3a6e97017e2f38aadc1486a85beff9ada79a61f
Borys Lopez <borislopez1976@hotmail.com>
2025-01-03 13:15:09 -0400

    Tipado de Datos

---
+++ sec1/tipado.js
@@ -0,0 +1,13 @@
+// Tipado de datos
+// String
+var nombre = "Borys";
+// Bolean 
+var abierto = false;
+// Number
+var dni = 26708053;
+// Array
+var persona = ["Borys", "Lopez"];
+var colleccionPersonas = [1, 2, 3, 4, 5];
+// Tuple
+var tupla = [23, "Borys", true];
+console.log(tupla);
---
+++ sec1/tipado.ts
@@ -0,0 +1,18 @@
+// Tipado de datos
+// String
+let nombre:string = "Borys";
+
+// Bolean 
+let abierto:boolean = false;
+
+// Number
+let dni:number = 26708053;
+
+// Array
+let persona:string[] = ["Borys", "Lopez"];
+let colleccionPersonas:Array<number> = [1,2,3,4,5];
+
+// Tuple
+let tupla:[number, string, boolean] =  [23, "Borys", true];
+
+console.log(tupla)
